90708d9eff4192dd178e6db1f23bf91916b4b01e
tw0517tw <tw0517tw@gmail.com>
2017-09-01 17:28:43 +0800

    add linter and precommit hook

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb-base', 'prettier'],
+  env: {
+    node: true,
+    jest: true,
+    jasmine: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    'no-param-reassign': 'off',
+    'no-return-assign': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+      },
+    ],
+  },
+};
